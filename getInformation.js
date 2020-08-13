7a283b37ac2a8f8f2aa1db128b7cca52f2d9bf6e
Lapi <tyoutugai.7@gmail.com>
2020-08-13 20:40:32 +0900

    feat: add all information

--- getInformation.js
+++ getInformation.js
@@ -1,5 +1,29 @@
-const tmp = document
-  .getElementsByTagName("h1")[0]
-  .innerText.split(/\r\n|\r|\n/);
-const name = tmp[tmp.length - 1];
-window.open("https://www.furport.tk/events/new?name=" + name, "_blank");
+const params = new URLSearchParams();
+
+let tmp = document.getElementsByTagName("h1")[0].innerText.split(/\r\n|\r|\n/);
+params.append("name", tmp[tmp.length - 1]);
+tmp = document
+  .querySelector(
+    "#container > tbody > tr > td.left_col.pad20 > p:nth-child(2) > a"
+  )
+  .getAttribute("href")
+  .split("/");
+params.append("twitter_id", tmp[tmp.length - 1]);
+tmp = document
+  .querySelector(
+    "#container > tbody > tr > td.left_col.pad20 > div:nth-child(14)"
+  )
+  .innerText.split(/\r\n|\r|\n/)[0];
+params.append("place", tmp);
+tmp = document
+  .querySelector(
+    "#container > tbody > tr > td.left_col.pad20 > div.arrow_box > span"
+  )
+  .innerText.match(/\d+/g);
+let date;
+tmp[3]
+  ? (date = new Date(tmp[0], tmp[1] - 1, tmp[2], tmp[3], tmp[4]))
+  : (date = new Date(tmp[0], tmp[1] - 1, tmp[2]));
+params.append("start_datetime", date.toISOString());
+params.append("end_datetime", date.toISOString());
+window.open("https://stg.furport.tk/events/new?" + params.toString(), "_blank");
